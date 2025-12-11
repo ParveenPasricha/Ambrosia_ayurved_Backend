01f8700dd6f079958426939bc4cbb3186e1d9223
Parveen Pasricha <184731702+ParveenPasricha@users.noreply.github.com>
2025-12-11 16:47:38 +0530

    add cors

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({ 
-    origin: '*',
+    origin: ['https://ambrosiaayurved.vercel.app',
+    "http://localhost:3000"],
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
