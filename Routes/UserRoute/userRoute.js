6346a1ab0485ca0616b3c7cffaf469289d14f244
Parveen Pasricha <184731702+ParveenPasricha@users.noreply.github.com>
2025-12-19 18:26:54 +0530

    Update code

--- Routes/UserRoute/userRoute.js
+++ Routes/UserRoute/userRoute.js
@@ -1,9 +1,7 @@
 const router = require("express").Router();
 const { requestOtp, verifyOtp, completeSignup } = require("../../Controller/UserController/userController");
-const productRoutes = require('../AdminRoute/productRoute');
 
 router.post("/request-otp", requestOtp);      
 router.post("/verify-otp", verifyOtp);         
 router.post("/complete-signup", completeSignup); 
-router.use('/products', productRoutes);
 module.exports = router;
